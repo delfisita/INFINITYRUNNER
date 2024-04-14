4d72981de5756f3d129289ea8fbbd05318a05612
delfisita <131388243+delfisita@users.noreply.github.com>
2024-04-14 13:46:14 -0300

    Add files via upload

---
+++ Mainmenu.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+public class Mainmenu : MonoBehaviour
+{
+    [SerializeField] GameObject mainPanel;
+    [SerializeField] GameObject instructionsPanel;
+    [SerializeField] GameObject optionsPanel;
+
+
+    public void GoToMain()
+    {
+        mainPanel.SetActive(true);
+        instructionsPanel.SetActive(false);
+        optionsPanel.SetActive(false);
+    }
+
+    public void GoToInstruciotns()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+    }
+
+    public void GoToOptions()
+    {
+        mainPanel.SetActive(false);
+        instructionsPanel.SetActive(false);
+        optionsPanel.SetActive(true);
+    }
+
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+
+    List<int> widths = new List<int>() { 568, 960, 1280, 1920 };
+    List<int> heights = new List<int>() { 329, 540, 800, 1080 };
+
+    public void SetScreenSize(int index)
+    {
+        bool fullscreen = Screen.fullScreen;
+        int width = widths[index];
+        int height = heights[index];
+        Screen.SetResolution(width, height, fullscreen);
+    }
+
+    public void SetFullscreen(bool _fullscreen)
+    {
+        Screen.fullScreen = _fullscreen;
+    }
+
+
+
+
+}
